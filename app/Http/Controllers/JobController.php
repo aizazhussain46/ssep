ebd574312f8daa0ac2358f9c61bff279c167eb4f
aizazhussain46 <aizaz.hussain@orangeroomdigital.com>
2020-10-15 13:26:22 +0500

    null again

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -172,7 +172,7 @@ class JobController extends Controller
         
         $job = Job::find($id);
        
-        if($job->assigned_to == null && $request->assigned_to){
+        if($job->assigned_to == null && $request->assigned_to != null){
            
             $arr['status_id'] = 3;
             $arr['assigned_to'] = $request->assigned_to;
