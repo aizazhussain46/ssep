4a679f5037dab67035fe0022006f3678e0987dee
aizazhussain46 <aizaz.hussain@orangeroomdigital.com>
2021-03-19 16:46:22 +0500

    fieldactivity

--- app/Fieldactivity.php
+++ app/Fieldactivity.php
@@ -7,4 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 class Fieldactivity extends Model
 {
     protected $guarded = [];
+
+    public function getImagesAttribute($value)
+    {
+        return $this->attributes['images'] = json_decode($value);
+    }
+
+    public function setImagesAttribute($value)
+    {
+        return $this->attributes['images'] = json_encode($value);
+    }
+
+    public function getAttendanceSheetAttribute($value)
+    {
+        return $this->attributes['attendance_sheet'] = json_decode($value);
+    }
+
+    public function setAttendanceSheetAttribute($value)
+    {
+        return $this->attributes['attendance_sheet'] = json_encode($value);
+    }
 }
